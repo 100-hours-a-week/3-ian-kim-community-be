32755ff7aa685e704c611bbd186d89f2971fdacf
gnsdp <rladpgns99@gmail.com>
2025-11-23 14:42:47 +0900

    chore: 미사용 클래스 삭제

--- src/main/java/ktb3/full/community/repository/PostImageRepository.java
+++
@@ -1,7 +0,0 @@
-package ktb3.full.community.repository;
-
-import ktb3.full.community.domain.entity.PostImage;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface PostImageRepository extends JpaRepository<PostImage, Long> {
-}
