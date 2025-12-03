7d8c21ee9af2082198f970ee704b2c196c0c2ade
gnsdp <rladpgns99@gmail.com>
2025-12-03 10:38:21 +0900

    test: PostApiController 슬라이스 테스트

---
+++ src/test/java/ktb3/full/community/ControllerTestSupport.java
@@ -0,0 +1,51 @@
+package ktb3.full.community;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ktb3.full.community.presentation.controller.AuthenticatedUserApiController;
+import ktb3.full.community.presentation.controller.CommentApiController;
+import ktb3.full.community.presentation.controller.PostApiController;
+import ktb3.full.community.presentation.controller.UserApiController;
+import ktb3.full.community.service.*;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
+import org.springframework.test.web.servlet.MockMvc;
+
+@ActiveProfiles("test")
+@WebMvcTest(controllers = {
+        AuthenticatedUserApiController.class,
+        UserApiController.class,
+        PostApiController.class,
+        CommentApiController.class,
+})
+public abstract class ControllerTestSupport {
+
+    @Autowired
+    protected MockMvc mockMvc;
+
+    @Autowired
+    protected ObjectMapper objectMapper;
+
+    @MockitoBean
+    protected UserService userService;
+
+    @MockitoBean
+    protected UserDeleteService userDeleteService;
+
+    @MockitoBean
+    protected PostService postService;
+
+    @MockitoBean
+    protected PostDeleteService postDeleteService;
+
+    @MockitoBean
+    protected CommentService commentService;
+
+    @MockitoBean
+    protected PostLikeService postLikeService;
+
+    @MockitoBean
+    protected ImageUploadService imageUploadService;
+}
---
+++ src/test/java/ktb3/full/community/presentation/controller/PostApiControllerTest.java
@@ -0,0 +1,286 @@
+package ktb3.full.community.presentation.controller;
+
+import ktb3.full.community.ControllerTestSupport;
+import ktb3.full.community.config.WithAuthMockUser;
+import ktb3.full.community.dto.response.PostDetailResponse;
+import ktb3.full.community.dto.response.PostResponse;
+import ktb3.full.community.fixture.MultipartFileFixture;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.HttpMethod;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.util.List;
+
+import static org.mockito.BDDMockito.given;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WithAuthMockUser
+class PostApiControllerTest extends ControllerTestSupport {
+
+    @Nested
+    class getAllPosts {
+
+        @Test
+        void 게시글_목록_페이지를_조회한다() throws Exception {
+            // given
+            PageRequest request = PageRequest.of(0, 10);
+            PagedModel<PostResponse> result = new PagedModel<>(new PageImpl<>(List.of()));
+
+            given(postService.getAllPosts(request)).willReturn(result);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(get("/posts")
+                    .param("page", "0")
+                    .param("size", "10"));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data.content").isArray())
+                    .andExpect(jsonPath("$.data.page").isMap());
+        }
+    }
+
+    @Nested
+    class getPostDetail {
+
+        @Test
+        void 게시글_상세정보를_조회한다() throws Exception {
+            // given
+            long userId = 1L;
+            long postId = 1L;
+            PostDetailResponse result = PostDetailResponse.builder().build();
+
+            given(postService.getPost(userId, postId)).willReturn(result);
+
+            // when
+            ResultActions resultActions = mockMvc.perform(get("/posts/{postId}", postId));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isNotEmpty());
+        }
+    }
+
+    @Nested
+    class createPost {
+
+        @Test
+        void 새_게시글을_작성한다() throws Exception {
+            // given
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/posts")
+                    .file(MultipartFileFixture.createImage())
+                    .param("title", "post title")
+                    .param("content", "post content")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isCreated())
+                    .andExpect(header().exists("Location"))
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 새_게시글_작성_시_제목에_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
+            // given
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/posts")
+                    .file(MultipartFileFixture.createImage())
+                    .param("title", " ")
+                    .param("content", "post content")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("제목은 필수입니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 새_게시글_작성_시_내용에_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
+            // given
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.POST, "/posts")
+                    .file(MultipartFileFixture.createImage())
+                    .param("title", "post title")
+                    .param("content", " ")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("내용은 필수입니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+    }
+
+    @Nested
+    class updatePost {
+
+        @Test
+        void 게시글의_제목만_수정한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.PATCH, "/posts/{postId}", postId)
+                    .param("title", "updated title")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 게시글의_내용만_수정한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.PATCH, "/posts/{postId}", postId)
+                    .param("content", "updated content")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 게시글의_이미지만_수정한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.PATCH, "/posts/{postId}", postId)
+                    .file(MultipartFileFixture.createImage())
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 게시글_수정_시_제목이_입력됐다면_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.PATCH, "/posts/{postId}", postId)
+                    .param("title", " ")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("제목은 공백일 수 없습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+
+        @Test
+        void 게시글_수정_시_내용이_입력됐다면_공백이_아닌_문자가_1개_이상_있어야_한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(multipart(HttpMethod.PATCH, "/posts/{postId}", postId)
+                    .param("content", " ")
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isBadRequest())
+                    .andExpect(jsonPath("$.code").value("4001"))
+                    .andExpect(jsonPath("$.message").value("내용은 공백일 수 없습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+    }
+
+    @Nested
+    class deletePost {
+
+        @Test
+        void 게시글을_삭제한다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(delete("/posts/{postId}", postId)
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+    }
+
+    @Nested
+    class likePost {
+
+        @Test
+        void 게시글에_좋아요를_누른다() throws Exception {
+            // given
+            long postId = 1L;
+
+            // when
+            ResultActions resultActions = mockMvc.perform(post("/posts/{postId}/like", postId)
+                    .with(csrf()));
+
+            // then
+            resultActions
+                    .andDo(print())
+                    .andExpect(status().isOk())
+                    .andExpect(jsonPath("$.code").isEmpty())
+                    .andExpect(jsonPath("$.message").value("요청에 성공했습니다."))
+                    .andExpect(jsonPath("$.data").isEmpty());
+        }
+    }
+}
