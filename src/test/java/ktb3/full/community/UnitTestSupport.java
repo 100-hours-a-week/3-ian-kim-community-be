4956a0495023073d52021294c6bf69008ac8a004
gnsdp <rladpgns99@gmail.com>
2025-12-06 14:22:14 +0900

    test: 단위 테스트 support 클래스 생성

---
+++ src/test/java/ktb3/full/community/UnitTestSupport.java
@@ -0,0 +1,8 @@
+package ktb3.full.community;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+public abstract class UnitTestSupport {
+}
