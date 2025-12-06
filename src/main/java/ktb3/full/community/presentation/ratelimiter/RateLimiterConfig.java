28d732edf9644f67e295d672e5efde3815bb22ba
gnsdp <rladpgns99@gmail.com>
2025-12-06 14:18:37 +0900

    feat: rate limit cache 빈 생성

---
+++ src/main/java/ktb3/full/community/presentation/ratelimiter/RateLimiterConfig.java
@@ -0,0 +1,24 @@
+package ktb3.full.community.presentation.ratelimiter;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import com.github.benmanes.caffeine.cache.Caffeine;
+import io.github.bucket4j.Bucket;
+import lombok.NonNull;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.util.concurrent.TimeUnit;
+
+@EnableConfigurationProperties(RateLimiterProperties.class)
+@Configuration
+public class RateLimiterConfig {
+
+    @Bean
+    public Cache<@NonNull String, Bucket> rateLimitCache(RateLimiterProperties props) {
+        return Caffeine.newBuilder()
+                .maximumSize(props.getCache().getMaximumSize())
+                .expireAfterAccess(props.getCache().getExpireAfterAccess(), TimeUnit.MILLISECONDS)
+                .build();
+    }
+}
