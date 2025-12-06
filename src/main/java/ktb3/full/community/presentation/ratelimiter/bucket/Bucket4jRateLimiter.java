66e9fa9c1e726ff499e09d4c03a54feaf3091862
gnsdp <rladpgns99@gmail.com>
2025-12-06 14:20:46 +0900

    feat: Bucket4jRateLimiter 구현체 생성

---
+++ src/main/java/ktb3/full/community/presentation/ratelimiter/bucket/Bucket4jRateLimiter.java
@@ -0,0 +1,26 @@
+package ktb3.full.community.presentation.ratelimiter.bucket;
+
+import com.github.benmanes.caffeine.cache.Cache;
+import io.github.bucket4j.Bucket;
+import ktb3.full.community.presentation.ratelimiter.RateLimiter;
+import lombok.Getter;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.Objects;
+
+@Getter
+@RequiredArgsConstructor
+@Service
+public class Bucket4jRateLimiter implements RateLimiter {
+
+    private final Cache<@NonNull String, Bucket> cache;
+    private final BucketFactory bucketFactory;
+
+    @Override
+    public boolean allowRequest(String clientKey, long numTokensToConsume) {
+        Bucket bucket = Objects.requireNonNull(cache.get(clientKey, key -> bucketFactory.createBucket()));
+        return bucket.tryConsume(numTokensToConsume);
+    }
+}
